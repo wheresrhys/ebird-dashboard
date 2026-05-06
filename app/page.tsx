3c636be1ad850d63eb28e72100232189c3a71adb
Rhys Evans <rhys.evans@ft.com>
2026-05-06 21:23:30 +0100

    add year and all time lists

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,15 @@
+import { getTicksList } from '../lib/ticks-list';
+import { createDb } from '../models/create-db';
+import { getYear, getAll } from '../models/load-csv';
+import { Temporal } from 'temporal-polyfill';
+
+
 export default function Home() {
+  const allTimeDb = createDb(getAll());
+  const allTimeTicks = getTicksList(allTimeDb);
+  const thisYearDb = createDb(getYear(new Date().getFullYear() - 1));
+  const thisYearTicks = getTicksList(thisYearDb);
+
   return (
     <div>
       <h1>ebird dashboard</h1>
@@ -30,10 +41,29 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <div className="flex">
+      <div className="w-half">
+        <h2>Year list</h2>
+        <ol className="list-inside list-decimal">
+          {/* TODO: have some concept of how special a bird is */}
+          {thisYearTicks.map(tick => (
+            <li className="mb-2" key={tick.submissionId}>
+              {tick.species.commonName} - {tick.date.toLocaleDateString()} - {tick.location.location}
+            </li>
+          ))}
+        </ol>
+      </div>
+      <div className="w-half">
+        <h2>Life list</h2>
+        <ol className="list-inside list-decimal">
+          {allTimeTicks.map(tick => (
+            <li className="mb-2" key={tick.submissionId}>
+                      {tick.species.commonName} - {tick.date.toLocaleDateString()} - {tick.location.location}
+                    </li>
+                  ))}
+        </ol>
+      </div>
+      </div>
     </div>
   );
 }
-
-
-
-
