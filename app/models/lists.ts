b0053161ecb068db4f9dd22f592f481515a911df
Rhys Evans <rhys.evans@ft.com>
2026-05-12 22:40:08 +0100

    low carbon list implemented

--- app/models/lists.ts
+++ app/models/lists.ts
@@ -1,4 +1,86 @@
 import { EbirdDataFilter } from "../lib/data-filters"
+import { County, LocationId, SubmissionId } from "./types"
+const lowCarbonCounties: County[] = [
+'East Sussex',
+'London',
+'Kent',
+'Essex',
+'Suffolk',
+'Hertfordshire',
+'Surrey',
+'West Sussex']
+
+const lowCarbonLocations: LocationId[] = [
+'L13996145',
+'L15697593',
+'L39903188',
+'L39867202',
+'L10342847',
+'L17212012',
+'L19601252',
+'L46652850'
+]
+
+const lowCarbonSubmissions: SubmissionId[] = [
+'S171491952',
+'S171448048',
+'S171143507',
+'S171254934',
+'S171003718',
+'S170805298',
+'S170769654',
+'S170750215',
+'S169936393',
+'S152014186',
+'S152005838',
+'S151966038',
+'S151965975',
+'S262545412',
+'S94821745',
+  'S113826753',
+  'S260503815',
+  'S260505502',
+  'S152014186',
+  'S260506079'
+]
+
+// reserve for truly hard to get to places I'm never likely to go to without a car
+const nonLowCarbonLocations: LocationId[] = [
+  'L970514'
+]
+
+const nonLowCarbonSubmissions: SubmissionId[] =[
+  'S126717146',
+  'S126624824',
+  'S312297119',
+  'S310654152',
+  'S111147025',
+  'S310674805',
+  'S111146970',
+  'S311275417',
+  'S88748962',
+  'S220040231',
+  'S111146933'
+]
+
+function isLowCarbon({
+  submissionId,
+  county,
+  locationId,
+}: {
+  submissionId: SubmissionId,
+  county: County,
+  locationId: LocationId,
+}) {
+  if (nonLowCarbonSubmissions.includes(submissionId)) {
+    return false;
+  }
+
+  if (nonLowCarbonLocations.includes(locationId)) {
+    return false
+  }
+  return lowCarbonCounties.includes(county) || lowCarbonLocations.includes(locationId) || lowCarbonSubmissions.includes(submissionId)
+}
 
 type ListConfig = {
   id: string,
@@ -10,6 +92,10 @@ export const listConfigMap: Record<string, Omit<ListConfig, 'id'>> = {
   uk: {
     name: 'UK',
     filters: []
+  },
+  lowCarbon: {
+    name: 'Low Carbon',
+    filters: [isLowCarbon]
   },
   london: {
     name: 'London',
